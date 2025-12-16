df5887c53b78f1eb8dcdfac002d6206c50b2e630
kalyaniaalla <kalyaniaalla28@gmail.com>
2025-12-16 17:21:55 +0530

    updated cors

--- chatapplication/settings.py
+++ chatapplication/settings.py
@@ -159,13 +159,13 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://chatapp-01yt.onrender.com",
+    "https://backend-company-task3.onrender.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://chatapp-01yt.onrender.com",
+    "https://backend-company-task3.onrender.com",
 ]
 
 CORS_ALLOW_HEADERS = [
